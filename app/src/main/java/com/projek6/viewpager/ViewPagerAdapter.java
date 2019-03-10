8ebb47ec7e71a151f053e1525eae1625e0e0f053
AhmadFikria <fikriadi77@gmail.com>
2019-03-10 09:43:34 +0700

    Initial commit

---
+++ app/src/main/java/com/projek6/viewpager/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.projek6.viewpager;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0){
+            return new FirstFragment();
+        }else if (position == 1){
+            return new SecondFragment();
+        }
+        throw new IllegalStateException("Position not Valid");
+
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0 || position % 2 == 0){
+            return "First";
+        }else if (position % 2 == 1){
+            return "Second";
+        }
+        throw new IllegalStateException("Position not Valid");
+    }
+}
